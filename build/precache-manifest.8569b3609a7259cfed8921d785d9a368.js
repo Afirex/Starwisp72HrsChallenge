81140784e749d28125116b2d24e8acf2db4e5850
Aftab Mohammad <aftabmohammad75@gmail.com>
2020-09-05 10:54:23 +0530

    production build updated

---
+++ build/precache-manifest.8569b3609a7259cfed8921d785d9a368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff53276ded722d78b2a31fc77232e6a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2e906fe9e3eaf6b03ad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1cd65fdcc8ffd15601ab",
+    "url": "/static/js/2.10506621.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10506621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e906fe9e3eaf6b03ad",
+    "url": "/static/js/main.18b8994d.chunk.js"
+  },
+  {
+    "revision": "3f0fdc21870e63ba8174",
+    "url": "/static/js/runtime-main.f59316c5.js"
+  }
+]);
